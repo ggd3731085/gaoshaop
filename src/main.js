615b1f926033bb0a4a46ec7f9ec8146afe3b5004
ggd3731085 <ggd3731@gmail.com>
2020-11-29 21:53:21 +0900

    lodingを追加

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Loading from './components/loading'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
-
+Vue.use(Loading)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: require('./assets/images/err.png'),
+  loading: require('./assets/images/loading.gif'),
+  attempt: 1,
+  listenEvents: ['scroll']
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
