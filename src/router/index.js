c3e587527dd09ba8cd5c16a0577d58bc968e3e4c
ggd3731085 <ggd3731@gmail.com>
2020-11-29 21:10:28 +0900

    index.js を修正

--- src/router/index.js
+++ src/router/index.js
@@ -6,37 +6,8 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/home',
+    path: '/',
+    name: 'Home',
     component: Home
-  }, {
-      path: '/catgory',
-      component: Category
-  }, {
-      path: '/cart',
-      component: Cart
-  }, {
-      path: '/search',
-      component: SearchPage
-  }, {
-      path: '/mine',
-      component: Mine
-  }, {
-      path: '/login',
-      component: Login
-  }, {
-      path: '/register',
-      component: Reg
-  }, {
-      path: '/catgory/:id',
-      component: Category
-  }, {
-      path: '/detail/:id',
-      component: GoodsDetail
-  }, {
-      path: '/',
-      redirect: '/home'
-  }, {
-      path: '*',
-      redirect: '/home'
   }]
 })
