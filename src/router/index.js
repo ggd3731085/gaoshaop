cc8ceb447b23f9f528eaa89ac90e23315ae6b928
ggd3731085 <ggd3731@gmail.com>
2020-11-29 20:48:19 +0900

    router.config.js を削除 router/index.js を戻る

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import routerconf from './router.config'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  scorllBehavior: () => ({
-      y: 0
-  }),
-  routerconf
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
