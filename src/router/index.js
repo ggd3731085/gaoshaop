864ab0141206074a20cdc9912e29d9d4339d9f75
ggd3731085 <ggd3731@gmail.com>
2020-11-29 20:59:38 +0900

    Home.vue を追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
+    path: '/home',
+    component: Home
+  }, {
+      path: '/catgory',
+      component: Category
+  }, {
+      path: '/cart',
+      component: Cart
+  }, {
+      path: '/search',
+      component: SearchPage
+  }, {
+      path: '/mine',
+      component: Mine
+  }, {
+      path: '/login',
+      component: Login
+  }, {
+      path: '/register',
+      component: Reg
+  }, {
+      path: '/catgory/:id',
+      component: Category
+  }, {
+      path: '/detail/:id',
+      component: GoodsDetail
+  }, {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+      redirect: '/home'
+  }, {
+      path: '*',
+      redirect: '/home'
+  }]
 })
