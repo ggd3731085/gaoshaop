1374a52a96e64fee436fb7355e00e25ff8d60421
ggd3731085 <ggd3731@gmail.com>
2020-12-01 00:21:19 +0900

    login を追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home.vue'
+import Login from '@/components/Login.vue'
 
 Vue.use(Router)
 
@@ -13,5 +14,9 @@ export default new Router({
     path: '/',
     name: 'Home',
     component: Home
+  }, {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }]
 })
