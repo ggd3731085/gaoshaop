3494b7a234681b0c8708ddfd4d444d38b1b60cb8
ggd3731085 <ggd3731@gmail.com>
2020-12-01 01:03:38 +0900

    db を追加

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,7 @@
 	<div class="my_l">
         <header class="top_bar">
 		    <a onclick="window.history.go(-1)" class="icon_back"></a>
-		    <h3 class="cartname">京东登录</h3>
+		    <h3 class="cartname">登录</h3>
 		</header>
 		<main class="user_login_box">
             <div class="login_dialog">
---
+++ src/settings.js
@@ -0,0 +1,9 @@
+/* 数据库连接 */
+module.exports = {
+  cookieSecret: 'gaoshop',
+  host: 'us-cdbr-east-02.cleardb.com',
+  port: 3306,
+  user: 'bd2d44922d1d08',
+  password: 'fd823d58',
+  db: 'heroku_1ac608ddb272ecf'
+}; 
