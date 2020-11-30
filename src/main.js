64fdc6cda1fe3ee854ae6539ca321fa41e11e566
ggd3731085 <ggd3731@gmail.com>
2020-11-30 23:40:59 +0900

    http を指定

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import Loading from './components/loading'
 import VueLazyload from 'vue-lazyload'
 import axios from 'axios'
+import store from './store/'
 
 Vue.config.productionTip = false
 Vue.use(Loading)
@@ -16,13 +17,22 @@ Vue.use(VueLazyload, {
   attempt: 1,
   listenEvents: ['scroll']
 })
-//axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
-axios.interceptors.request.use(function(config) { //配置发送请求的信息
+// axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
+axios.interceptors.request.use(function (config) { // 配置发送请求的信息
   store.dispatch('showLoading')
   return config
-}, function(error) {
+}, function (error) {
   return Promise.reject(error)
 })
+axios.interceptors.response.use(function (response) { // 配置请求回来的信息
+  store.dispatch('hideLoading')
+  return response
+}, function (error) {
+  return Promise.reject(error)
+})
+axios.defaults.baseURL = process.env.PORT || 8080
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
