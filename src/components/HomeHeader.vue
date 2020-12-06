051eddfb8ed48261a2af5451be0488e9aceb4efc
ggd3731085 <ggd3731@gmail.com>
2020-12-06 12:12:44 +0900

    ログインボタンの位置

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -3,10 +3,10 @@
         <header class="m_header">
                 <div class="m_header_box" id="my_search">
                     <a href="#" class="icon_logo"></a>
-                    <form action="#">
+                    <!--<form action="#">
                         <span class="icon_search"></span>
                         <input type="search" class="search" placeholder="検索" @click="goSearch($event)">
-                    </form>
+                    </form>-->
                     <router-link to="/login" class="logo_btn">login</router-link>
                 </div>
             </header>
