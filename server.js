d98e5d3d79e17927d5e13eddd3c4ede97928953b
ggd3731085 <ggd3731@gmail.com>
2020-12-09 00:52:21 +0900

    503解決

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // これでルート以外でリロードしてもindex.htmlを読み込んでちゃんとルーティングをしてくれる
 // /.*/で全てのルートを指定。req(request), res(response)
 app.get(/.*/, function(req, res,next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     res.sendFile(__dirname + "/dist/index.html");
     if (req.method == 'OPTIONS') {
         res.send(200);
