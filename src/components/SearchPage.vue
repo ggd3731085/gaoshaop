7da00ec0dce3317461635c69fd808408b6b5d2d4
ggd3731085 <ggd3731@gmail.com>
2020-12-31 01:46:48 +0900

    資格検索2

--- src/components/SearchPage.vue
+++ src/components/SearchPage.vue
@@ -1,158 +1,72 @@
 <template>
 <div id="app">
   <v-app id="inspire">
-    <!--DataTable-->
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      sort-by="calories"
-      class="elevation-1"
-    >
-      <!--v-slot:top-->
-      <template v-slot:top>
-        <!--v-toolbar-->
-        <v-toolbar flat>
-          <!--v-toolbar-title-->
-          <v-toolbar-title>資格検索</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          ></v-divider>
-          <v-spacer></v-spacer>
-          <!--v-dialog-->
-          <v-dialog
-            v-model="dialog"
-            max-width="500px">
-            <!--新規ボタン-->
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                dark
-                class="mb-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                新規
-              </v-btn>
-            </template>
-            <!--v-card--->
-            <v-card>
-              <!--v-card-title--->
-              <v-card-title>
-                <span class="headline">{{ formTitle }}</span>
-              </v-card-title>
-              <!--v-card-text-->
-              <v-card-text>
-                <!--行列のコンテナ-->
-                <v-container>
-                  <!--行-->
-                  <v-row>
-                    <!--列1 employee_id-->
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.employee_id"
-                        label="employee_id"
-                      ></v-text-field>
-                    </v-col>
-                    <!--列2 certification_name-->
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.certification_name"
-                        label="certification_name"
-                      ></v-text-field>
-                    </v-col>
-                    <!--列3 get_date-->
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.get_date"
-                        label="get_date"
-                      ></v-text-field>
-                    </v-col>
-                    <!--列4 encourage_date-->
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.encourage_date"
-                        label="encourage_date"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-              <!--v-card-actionsキャンセル、ボタン-->
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="close"
-                >
-                  取消
-                </v-btn>
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="save"
-                >
-                  保存
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon
-          small
-          class="mr-2"
-          @click="editItem(item)"
-        >
-          mdi-pencil
-        </v-icon>
-        <v-icon
-          small
-          @click="deleteItem(item)"
-        >
-          mdi-delete
-        </v-icon>
-      </template>
-      <template v-slot:no-data>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    sort-by="calories"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>社員資格</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+       
         <v-btn
           color="primary"
-          @click="initialize"
+          dark
+          class="mb-2"
+          @click="newItem()"
         >
-          Reset
+          新規
         </v-btn>
-      </template>
-    </v-data-table>
-  </v-app>
+         
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+    </v-app>
 </div>
 </template>
 <script src="./js/searchPage.js"></script>
+
--- src/components/js/searchPage.js
+++ src/components/js/searchPage.js
@@ -1,134 +1,121 @@
-export default {
-  data: () => ({
-    dialog: false,
-    dialogDelete: false,
-    headers: [
-      {
-        text: 'employee_id',
-        align: 'start',
-        sortable: false,
-        value: 'employee_id'
+  export default {
+    data:() => ({
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: '名前',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: '社員ID', value: 'employee_id' },
+        { text: 'フリガナ', value: 'frigana' },
+        { text: '入社年月日', value: 'entering_date' },
+        { text: '資格', value: 'certification_name' },
+        { text: '取得年月日', value: 'get_date' },
+        { text: '奨励金日付', value: 'encourage_date' },
+        { text: '操作', value: 'actions', sortable: false },
+      ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
       },
-      { text: 'certification_name', value: 'certification_name' },
-      { text: 'get_date', value: 'get_date' },
-      { text: 'encourage_date', value: 'encourage_date' },
-      { text: 'Actions', value: 'actions', sortable: false }
-    ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      employee_id: '',
-      certification_name: '',
-      get_date: '',
-      encourage_date: ''
+      defaultItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
     },
-    defaultItem: {
-      employee_id: '',
-      certification_name: '',
-      get_date: '',
-      encourage_date: ''
-    }
-  }),
 
-  computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? '新規' : '編集'
-    }
-  },
-
-  watch: {
-    dialog (val) {
-      val || this.close()
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
     },
-    dialogDelete (val) {
-      val || this.closeDelete()
-    }
-  },
 
-  created () {
-    this.initialize()
-  },
+    created () {
+      this.initialize()
+    },
 
-  methods: {
-    initialize () {
-      let _this = this
-      _this.$http.get('/searchEmployeeCertification', {
-      }).then((res) => {
-        if (res.status === 200) {
-          _this.desserts = res.data
-        } else {
-          alert('エラーが発生しました')
+    methods: {
+      initialize () {
+          let _this = this;
+  
+          _this.$http.get('/employeeCertifications').then((res)=>{
+        if(res.status == 200){
+          console.log('res.data ', res.data)
+          this.desserts = res.data;
+        }else{
+            alert('エラーが発生しました。');
         }
-        console.log(res)
-      }, (err) => {
-        console.log(err)
-      })
-    },
+            console.log(res);
+        },(err)=>{
+            alert('APIエラーが発生しました。');
+            console.log(err);
+        });
+      },
 
-    editItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
+      newItem () {
+        this.$emit('handleSwitch','input')
+      },
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        console.log(this.editedItem)
+        this.$emit('handleSwitch','input', this.editedItem)
+      },
 
-    deleteItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-    },
+      deleteItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
 
-    deleteItemConfirm () {
-      this.desserts.splice(this.editedIndex, 1)
-      this.closeDelete()
-    },
+      deleteItemConfirm () {
+        this.desserts.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
 
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    closeDelete () {
-      this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      } else {
-        this.desserts.push(this.editedItem)
-      }
-      let _this = this
-      if (_this.editedItem == null) {
-        alert('データを入力してください')
-      } else {
-        _this.$http.post('/saveEmployeeCertification', {
-          editedItem: _this.editedItem
-        }).then((res) => {
-          if (res.status === 200) {
-            _this.regInfo = res.data
-            if (_this.regInfo.status === 1) {
-              alert('保存が成功しました')
-            } else {
-              alert('保存が失敗しました')
-            }
-          } else {
-            alert('エラーが発生しました')
-          }
-          console.log(res)
-        }, (err) => {
-          console.log(err)
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
         })
-      }
-      console.log('save_end')
-      this.dialog = false
-      // this.close()
-    }
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.desserts.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
   }
-}
